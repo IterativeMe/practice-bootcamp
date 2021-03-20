66916671665a60e2e9b545731652e861c992c385
Maria Aguilera <marelynaguilera@gmail.com>
2021-03-20 17:32:47 -0500

    section 3: working with inputs

---
+++ tests/actions.js
@@ -0,0 +1,15 @@
+import { short } from "../lib/timeout"
+
+describe('Browser Actions',() => {
+    it('Inputs',() => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const input = $('#developer-name')
+        input.waitForExist()
+        input.setValue('Maria')
+        browser.pause(short)
+        input.clearValue()
+        browser.pause(short)
+        input.addValue('Aguilera')
+        browser.pause(short)
+    })
+})
