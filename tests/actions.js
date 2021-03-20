7fe1b1275357d0f9b2be85a5804645c0e9f9d10b
Maria Aguilera <marelynaguilera@gmail.com>
2021-03-20 17:58:53 -0500

    section 3: checkbox, selectbox and radio button

--- tests/actions.js
+++ tests/actions.js
@@ -1,36 +1,36 @@
 import { short } from "../lib/timeout"
 
 describe('Browser Actions',() => {
-    // it('Inputs',() => {
-    //     browser.url('https://devexpress.github.io/testcafe/example/')
-    //     const input = $('#developer-name')
-    //     input.waitForExist()
-    //     input.setValue('Maria')
-    //     browser.pause(short)
-    //     input.clearValue()
-    //     browser.pause(short)
-    //     input.addValue('Aguilera')
-    //     browser.pause(short)
-    // })
+    it('Inputs',() => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const input = $('#developer-name')
+        input.waitForExist()
+        input.setValue('Maria')
+        browser.pause(short)
+        input.clearValue()
+        browser.pause(short)
+        input.addValue('Aguilera')
+        browser.pause(short)
+    })
 
-    // it('Click',() => {
-    //     const buttonPopulate = $("#populate")
-    //     browser.waitForExist()
-    //     buttonPopulate.click()
-    // })
+    it('Click',() => {
+        const buttonPopulate = $("#populate")
+        browser.waitForExist()
+        buttonPopulate.click()
+    })
 
-    // it('Checkbox & Radio Button', () => {
-    //     browser.url('https://devexpress.github.io/testcafe/example/')
-    //     const radio = $('#linux')
-    //     radio.waitForExist()
-    //     radio.click()
-    //     browser.pause(short)
+    it('Checkbox & Radio Button', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/')
+        const radio = $('#linux')
+        radio.waitForExist()
+        radio.click()
+        browser.pause(short)
 
-    //     const checkbox = $('#remote-testing')
-    //     checkbox.waitForExist()
-    //     checkbox.click()
-    //     browser.pause(short)
-    // })
+        const checkbox = $('#remote-testing')
+        checkbox.waitForExist()
+        checkbox.click()
+        browser.pause(short)
+    })
 
     it('Select Box', () => {
         browser.url('https://devexpress.github.io/testcafe/example/')
